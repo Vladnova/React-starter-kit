6f526a3aa4d9346638ed3a9270f0cba06748589a
Vladislav Novalenko <novalenkov@ukr.net>
2021-02-24 00:11:22 +0200

    refactoring html

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <App /> */}
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
